381e0d4998e998d6aa6b6fc9a92b7baa107263bd
Dulitha-Pramod-Rajapakse <162677480+Dulitha-Pramod-Rajapakse@users.noreply.github.com>
2025-08-12 21:06:11 +0530

    Updated Layout

--- DTS_INFO/src/AppRouter.jsx
+++ DTS_INFO/src/AppRouter.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LocationWise from "./Components/LocationWise.jsx";
+import LocationWise from "./layouts/LocationWise.jsx";
 import DesignationWise from "./Components/DesignationWise.jsx";
+import Header from './Components/Header/Header.jsX';
 
 function AppRouter() {
   return (
     <Router>
+      <Header />
       <Routes>
         {/* <Route path="/" element={<DesignationWise />} /> */}
         <Route path="/" element={<LocationWise/>}/>
