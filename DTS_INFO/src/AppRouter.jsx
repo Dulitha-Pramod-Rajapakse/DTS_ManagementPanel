c933acf89aedce877efce227a8f3ddf7e3caba81
Ammaramd22 <ammaramd22@gmail.com>
2025-08-13 20:25:21 +0530

    Daily Deployment expanded and employees part created

--- DTS_INFO/src/AppRouter.jsx
+++ DTS_INFO/src/AppRouter.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LocationWise from "./Components/LocationWise.jsx";
 import DesignationWise from "./Components/DesignationWise.jsx";
 import DailyDeployment from "./layouts/DailyDeployment.jsx";
+import EmployeeList from "./Components/EmployeeList.jsx";
 import Header from "./Components/Header/Header.jsx"; 
 
 function AppRouter() {
@@ -12,7 +13,8 @@ function AppRouter() {
       <Routes>
         {/* <Route path="/" element={<DesignationWise />} /> */}
         <Route path="/" element={<LocationWise/>}/>
-        <Route path="/daily-deployment" element={<DailyDeployment />} />
+        <Route path="/dailyDeployment" element={<DailyDeployment />} />
+        <Route path="/employees/:code" element={<EmployeeList />} />
         {/* Add more routes here when needed */}
       </Routes>
     </Router>
--- DTS_INFO/src/layouts/DailyDeployment.jsx
+++ DTS_INFO/src/layouts/DailyDeployment.jsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import './DailyDeployment.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import DailyDeploymentTable from '../Components/DailyDeploymentTable.jsx';
+import DailyDeploymentEx from '../Components/DailyDeploymentEx.jsx';
 
 export default function DailyDeployment() {
   return (
     <div>
       <h1 className="Topic">Daily Deployment</h1>
       <DailyDeploymentTable />
+      <DailyDeploymentEx />
     </div>
   );
 }
