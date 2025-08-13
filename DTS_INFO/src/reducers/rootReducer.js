08cb4a883c539808a0351deb0e6f3de3f86541ac
Yasas Arandara <143877390+Ellio-Dredd@users.noreply.github.com>
2025-08-13 22:45:44 +0530

    Update rootReducer.js

--- DTS_INFO/src/reducers/rootReducer.js
+++ DTS_INFO/src/reducers/rootReducer.js
@@ -1,13 +1,15 @@
 
 import { combineReducers } from "redux";
 import { GetCurrentTime } from "./timeReducers";
-import { attendanceReducer } from "./attendanceReducer";
+import { attendanceReducer } from "./attendanceReducer"
+import { locationReducer } from "./locationReducer"
 import { loginReducer } from "./loginReducer"; // Assuming you have a loginReducer
 
 const rootReducer = combineReducers({
     GetCurrentTime,
     attendance: attendanceReducer,
     login:loginReducer,
+    locationData: locationReducer
 });
 
 export default rootReducer;
